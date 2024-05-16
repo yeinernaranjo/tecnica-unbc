87e047126eae369f778a63a624c58e4b184cc8c7
yeinernaranjo <yeinernaranjo@gmail.com>
2024-05-16 10:05:55 -0500

    Route Fix

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Livewire\Register;
 use App\Livewire\UsersManagement;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Login::class)->name('login');
+Route::get('/', Login::class)->name('home');
 Route::get('/login', Login::class)->name('login');
 Route::middleware('auth')->get('/users', UsersManagement::class);
 Route::get('/logout', 'App\Livewire\Login@logout')->name('logout');
