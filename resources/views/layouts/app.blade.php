d6ec44dc4dc50a96b601114945b18ff3aeb3571a
yeinernaranjo <47014622+yeinernaranjo@users.noreply.github.com>
2024-05-16 10:56:43 -0500

    Title Change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD - INBC</title>
+    <title>CRUD - UNBC</title>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
